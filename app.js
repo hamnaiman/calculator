ee6299dd507ece0e0a844bc70c1cb1f868e8d6b4
hamna iman <hamnajawaid22@gmail.com>
2024-08-06 00:32:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input
+}
+function clearDisplay() {
+    display.value = ""
+}
+
+function calculate() {
+   try{
+    display.value = eval(display.value);
+   }
+   catch(error){
+    display.value = "Error"
+   }
+   
+}
+function deleteDisplay(){
+     display.value = ""
+}
+
+
+
+
+
